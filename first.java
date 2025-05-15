3d17f41178134e170a19dc470f7b23a38675a2f9
Dhruv Vashistha <dvas7337@gmail.com>
2025-05-15 08:09:20 +0530

    adding the changes

--- first.java
+++ first.java
@@ -1,6 +1,6 @@
 class Main{
     public static void Main(string[] args){
         System.out.println("this is gonna be the first code on the github.")
-        System.out.println("this is gonna be the first code on the github.")
+        System.out.println("this is gonna be the first code on the (github).")
     }
 }
